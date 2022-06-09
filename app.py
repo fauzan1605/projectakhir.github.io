3e34a48970ff57348b2ed017cdfeef70a18c43af
Fauzan Azimah <you@example.com>
2022-06-09 08:56:21 +0700

    initial commit

--- app.py
+++ app.py
@@ -41,6 +41,9 @@ def generic():
     else:
         return "Unsupported Request Method"
 
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
